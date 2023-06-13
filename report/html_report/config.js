7a93ff49518ba899c2396d378ae2cb353c47c22b
Igor Novikov <novikov.igor.20@gmail.com>
2023-06-13 14:47:48 +0300

    make build

--- report/html_report/config.js
+++ report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Card_with_data-qa_card.png",
-        "test": "..\\bitmaps_test\\20230608-011702\\Card_with_data-qa_card.png",
+        "test": "..\\bitmaps_test\\20230612-194328\\Card_with_data-qa_card.png",
         "selector": "[data-qa=\"card\"]",
         "fileName": "Card_with_data-qa_card.png",
         "label": "Card with data-qa_card",
@@ -15,22 +15,22 @@ report({
         "expect": 0,
         "viewportLabel": "tablet_h",
         "diff": {
-          "isSameDimensions": false,
+          "isSameDimensions": true,
           "dimensionDifference": {
-            "width": 2,
-            "height": 4
+            "width": 0,
+            "height": 0
           },
-          "misMatchPercentage": "37.81",
-          "analysisTime": 13
+          "misMatchPercentage": "9.43",
+          "analysisTime": 15
         },
-        "diffImage": "..\\bitmaps_test\\20230608-011702\\failed_diff_Card_with_data-qa_card.png"
+        "diffImage": "..\\bitmaps_test\\20230612-194328\\failed_diff_Card_with_data-qa_card.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Link_with_data-qa_hover.png",
-        "test": "..\\bitmaps_test\\20230608-011702\\Link_with_data-qa_hover.png",
+        "test": "..\\bitmaps_test\\20230612-194328\\Link_with_data-qa_hover.png",
         "selector": "[data-qa=\"card\"]",
         "fileName": "Link_with_data-qa_hover.png",
         "label": "Link with data-qa_hover",
@@ -41,15 +41,15 @@ report({
         "expect": 0,
         "viewportLabel": "tablet_h",
         "diff": {
-          "isSameDimensions": false,
+          "isSameDimensions": true,
           "dimensionDifference": {
-            "width": 2,
-            "height": 4
+            "width": 0,
+            "height": 0
           },
-          "misMatchPercentage": "37.81",
-          "analysisTime": 16
+          "misMatchPercentage": "9.43",
+          "analysisTime": 14
         },
-        "diffImage": "..\\bitmaps_test\\20230608-011702\\failed_diff_Link_with_data-qa_hover.png"
+        "diffImage": "..\\bitmaps_test\\20230612-194328\\failed_diff_Link_with_data-qa_hover.png"
       },
       "status": "fail"
     }
